f30e82d8e221fea29b2abe334654b67ede678fb2
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-11-09 01:06:05 +0100

    fix

--- hombresLoboLaravel/database/migrations/2025_11_08_181556_create_roles_partidas_table.php
+++ hombresLoboLaravel/database/migrations/2025_11_08_181556_create_roles_partidas_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('roles__partidas', function (Blueprint $table) {
+        Schema::create('roles_partidas', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
