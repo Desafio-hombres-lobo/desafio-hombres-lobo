c5f6d007a2d73c7988cecdf4f3dca8e9e33f8cdc
Alfredo Carrero Muñiz <alfredoibros@gmail.com>
2025-11-12 14:09:06 +0100

    validacion formulario nickname
    Hecho con .js para probar en local, luego cambiar por .ts

--- hombreslobo-typescript/src/autenticacion/validarFormulario.ts
+++ hombreslobo-typescript/src/autenticacion/validarFormulario.ts
@@ -97,7 +97,7 @@ const validarCamposFormulario = (e: SubmitEvent): void => {
 };
 
 // Función para mostrar el mensaje de error
-const mostrarError = (input: HTMLInputElement, mensaje: string): void => {
+export const mostrarError = (input: HTMLInputElement, mensaje: string): void => {
   let errorSpan = input.parentElement?.querySelector<HTMLSpanElement>(".error");
 
   if (!errorSpan) {
@@ -111,7 +111,7 @@ const mostrarError = (input: HTMLInputElement, mensaje: string): void => {
 };
 
 // Función para limpiar el mensaje de error
-const limpiarError = (input: HTMLInputElement): void => {
+export const limpiarError = (input: HTMLInputElement): void => {
   const errorSpan =
     input.parentElement?.querySelector<HTMLSpanElement>(".error");
   if (errorSpan) errorSpan.textContent = "";
--- hombreslobo-typescript/src/perfil/ts/perfil.ts
+++ hombreslobo-typescript/src/perfil/ts/perfil.ts
@@ -1,1 +1,45 @@
 import "../css/perfil.css";
+import {
+  mostrarError,
+  limpiarError,
+} from "../../autenticacion/validarFormulario";
+
+const formCambiarNickname =
+  document.querySelector<HTMLFormElement>("#form-nickname");
+
+const nuevoNicknameUsuario =
+  formCambiarNickname?.querySelector<HTMLInputElement>(
+    'input[name="nuevo-nickname"]'
+  )!;
+
+formCambiarNickname?.addEventListener("submit", () => {
+  console.log("Botón cambiar nickname pulsado");
+
+  if (nuevoNicknameUsuario.value.length <= 0) {
+    alert("El nickname no puede estar vacío");
+    return;
+  } else {
+    alert("Nickname cambiado correctamente");
+  }
+});
+
+// const validarFormNickname = (): void => {
+//   if (!formCambiarNickname) return;
+
+//   formCambiarNickname.addEventListener("submit", validarCampoNickname);
+// };
+
+// const validarCampoNickname = (e: SubmitEvent): void => {
+//   e.preventDefault();
+
+//   const campo = nuevoNicknameUsuario.value;
+
+//   if (campo.length < 0) {
+//     mostrarError(
+//       nuevoNicknameUsuario,
+//       "El nuevo nickname no puede estar vacío."
+//     );
+//   } else {
+//     limpiarError(nuevoNicknameUsuario);
+//   }
+// };
