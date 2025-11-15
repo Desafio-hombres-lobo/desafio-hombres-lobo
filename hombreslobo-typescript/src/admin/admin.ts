3838d1d0aa216b745e506f6d6c255896f9ef697c
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-11-15 15:36:50 +0100

    get all usuarios en panel de admin

--- hombreslobo-typescript/src/admin/admin.ts
+++ hombreslobo-typescript/src/admin/admin.ts
@@ -1,17 +1,22 @@
 import "./css/admin.css";
-
+import {
+  cogerTodosLosUsuarios,
+  cogerUnUsuario,
+  borrarUsuario,
+  actualizarUsuario,
+} from "../providers/envioDatosAdmin";
 const SESSIONSTORAGE = "auth_token";
 const ROL_USUARIO = "auth_rol";
 const CLAVE_USUARIO = "auth_usuario";
 const LOCALSTORAGE = "credenciales";
 
-const btnVerUsuario = document.getElementById("btn-mostrar-todos");
-const btnEncontrarUsuario = document.getElementById("btn-buscar-uno");
-const btnBorrarUsuario = document.getElementById("btn-borrar-uno");
+const btnVerUsuario = document.getElementById("btn-mostrar-todos")!;
+const btnEncontrarUsuario = document.getElementById("btn-buscar-uno")!;
+const btnBorrarUsuario = document.getElementById("btn-borrar-uno")!;
 const entradaUsuario = document.getElementById(
   "input-usuario"
 ) as HTMLInputElement;
-const resultado = document.getElementById("salida-resultados");
+const resultado = document.getElementById("salida-resultados")!;
 
 const rolUsuario = sessionStorage.getItem(ROL_USUARIO);
 const panel = document.getElementById("panel-admin");
@@ -25,3 +30,18 @@ if (rolUsuario !== "admin" && panel) {
 } else {
   panel?.classList.remove("oculto");
 }
+
+//Api responses
+
+function mostrarResultados(data: any) {
+  if (resultado) resultado.textContent = JSON.stringify(data, null, 2); //El dos trabaja el indent del JSON para que se vea mas bonito
+}
+
+btnVerUsuario.addEventListener("click", async () => {
+  try {
+    const data = await cogerTodosLosUsuarios(); // 👈 Llama al provider
+    mostrarResultados(data);
+  } catch (error) {
+    mostrarResultados({ error: (error as Error).message });
+  }
+});
