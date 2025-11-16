d169ab248d256b21accd0d30dde93f2aa5ed6d8d
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-11-16 14:08:21 +0100

    Update api.php
    refactor de rutas, las agrupamos en dos, admin y usuario

--- hombresLoboLaravel/routes/api.php
+++ hombresLoboLaravel/routes/api.php
@@ -6,33 +6,33 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-    // Rutas de ADMIN (requieren token Y la habilidad 'admin')
-    Route::apiResource('users', UserController::class)
-        ->middleware('ability:admin');
+    // Rutas de ADMIN (requieren token y la habilidad 'admin')
+    Route::middleware('ability:admin')->group(function () {
 
+        Route::apiResource('users', UserController::class);
+        Route::get('/partidas', [PartidaController::class, 'index']);
+    });
+
+    //Rutas de usuario (requieren token y la habilidad 'usuario')
+    Route::middleware('ability:usuario')->group(function () {
+
+        # Cambiar nickname jugador
+        Route::post('/cambiarNicknameUsuario', [JugadorController::class, 'update']);
+
+        # Obtener Estadísticas Jugador
+        Route::get('/obtenerEstadisticasJugador', [JugadorController::class, 'show']);
+
+        # Cambiar Contraseña Jugador
+        Route::post('/cambiarPasswordJugador', [JugadorController::class, 'cambiarPassword']);
+
+        Route::get('/partidasIniciando', [PartidaController::class, 'partidasIniciando']);
+
+        Route::post('/crearPartida', [PartidaController::class, 'store']);
+
+    });
 });
 
 //Rutas publicas
 Route::post('/registrar', [AuthController::class, 'registrar']);
 Route::post('/login', [AuthController::class, 'loguear']);
-
-# Cambiar nickname jugador
-Route::post('/cambiarNicknameUsuario', [JugadorController::class, 'update'])
-    ->middleware('auth:sanctum');
-
-# Obtener Estadísticas Jugador
-Route::get('/obtenerEstadisticasJugador', [JugadorController::class, 'show'])
-    ->middleware('auth:sanctum');
-//  ->middleware(['auth:sanctum', 'ability:usuario']);
-
-# Cambiar Contraseña Jugador
-Route::post('/cambiarPasswordJugador', [JugadorController::class, 'cambiarPassword'])
-     ->middleware('auth:sanctum');
-
-Route::get('/partidas', [PartidaController::class, 'index']);
-
-Route::get('/partidasIniciando', [PartidaController::class, 'partidasIniciando']);
-
-Route::post('/crearPartida', [PartidaController::class, 'store'])
-    ->middleware('auth:sanctum');
 
